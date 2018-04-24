415fd3ee5a147d3a7a6f5c78959a9cc50b831d38
XiaYaLing <only_xyl@163.com>
2018-04-24 18:03:24 +0800

    修改pom

--- src/main/java/com/bmsmart/spring/boot/springboot/SpringbootApplication.java
+++ src/main/java/com/bmsmart/spring/boot/springboot/SpringbootApplication.java
@@ -1,8 +1,9 @@
 package com.bmsmart.spring.boot.springboot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * spring boot 启动类
  *
@@ -18,14 +19,14 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
  * @ComponentScan等组合在一下，非常简单，使用也方便
  */
 @SpringBootApplication(exclude = RedisAutoConfiguration.class)
-public class SpringbootApplication /*extends SpringBootServletInitializer*/{
+public class SpringbootApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+  /*  public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
-    }
+    }*/
 
-    /*@Override
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(SpringbootApplication.class);
-    }*/
+    }
 }
